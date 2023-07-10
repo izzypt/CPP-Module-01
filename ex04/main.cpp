97cfda2d8bec6762d516137d31fb57de3fc9c9ba
Simao <bonvalots@gmail.com>
2023-07-10 16:05:33 +0100

    fix to replace multiple ocurrences in the same line

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: simao <simao@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/10 13:00:02 by simao             #+#    #+#             */
-/*   Updated: 2023/07/10 15:33:19 by simao            ###   ########.fr       */
+/*   Updated: 2023/07/10 16:04:31 by simao            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,18 +53,18 @@ int	main(int argc, char **argv)
 		std::string line;
 		while (std::getline(file, line))
 		{
-			std::cout << "read line: " << line << std::endl;
 			str_pos = line.find(str1);
-			if (str_pos >= 0)
+			while (str_pos >= 0)
 			{
 				std::cout << "Found a match at: " << str_pos << std::endl;
 				line.insert(str_pos, str2);
 				line.erase((str_pos + str2.length()), str1.length());
+				str_pos = line.find(str1);
 			}
 			new_file << line << std::endl;
 		}
 		file.close();
 		new_file.close();
 	}
-
+	return (0);
 }
